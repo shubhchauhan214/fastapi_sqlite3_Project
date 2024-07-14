30c49db08128735754dc61c63c284157561cc41f
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-07-15 02:44:12 +0530

    Read all Contents

--- main.py
+++ main.py
@@ -10,23 +10,23 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-#
-#
-# db_dependency = Annotated[Session, Depends(get_db)]
-#
-#
-# # Read all contents
-# @app.get("/")
-# async def students(db: db_dependency):
-#     return db.query(School).all()
-#
-#
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+# Read all contents
+@app.get("/")
+async def students(db: db_dependency):
+    return db.query(Students).all()
+
+
 # # Get content using id
 # @app.get("/student/{student_id}")
 # async def student_by_id(db:db_dependency, student_id: int):

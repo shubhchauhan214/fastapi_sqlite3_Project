cc8eea59eb15a5d487c6c54548ee7131d067419c
shubhchauhan214 <shubhchauhan616@gmail.com>
2024-07-16 01:17:17 +0530

    PUT Request Method

--- main.py
+++ main.py
@@ -52,3 +52,22 @@ async def new_student(db: db_dependency, student_request: StudentRequest):
 
     db.add(student_model)
     db.commit()
+
+
+# PUT Request Method
+@app.put("/student/{student_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_student(db: db_dependency,
+                         student_id: int,
+                         student_request: StudentRequest):
+    student_model = db.query(Students).filter(Students.id == student_id).first()
+    if student_model is None:
+        raise HTTPException(status_code=404, detail='Student not found')
+
+    student_model.first_name = student_request.first_name
+    student_model.last_name = student_request.last_name
+    student_model.address = student_request.address
+
+    db.add(student_model)
+    db.commit()
+
+
